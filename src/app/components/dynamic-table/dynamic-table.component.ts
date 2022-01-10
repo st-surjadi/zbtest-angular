57597e552b3c2acf231e47dc97d3cfe4be941257
stevensurjadi1101 <stevensurjadi1101@gmail.com>
2022-01-10 11:59:31 +0700

    Create dynamic-table basic display.

--- src/app/components/dynamic-table/dynamic-table.component.ts
+++ src/app/components/dynamic-table/dynamic-table.component.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, Input, OnChanges, OnInit, SimpleChanges, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
 
 @Component({
   selector: 'app-dynamic-table',
   templateUrl: './dynamic-table.component.html',
   styleUrls: ['./dynamic-table.component.scss']
 })
-export class DynamicTableComponent implements OnInit {
+export class DynamicTableComponent implements OnInit, OnChanges {
+
+  @Input() data: any = null;
+  @Input() columns: Array<any> = [];
+  @Input() columnDisplayList: Array<any> = [];
+
+  @ViewChild(MatPaginator) paginator: MatPaginator;
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  ngAfterViewInit(): void {
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    if (changes["data"]) {
+      this.data.paginator = this.paginator;
+    }
   }
 
 }
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { MatTableDataSource } from '@angular/material/table';
+import { ApiService } from 'src/app/services/api.service';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +9,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  studentData = new MatTableDataSource<any>();
+  studentTableColumns = ['students', 'school_origin', 'school_correcting', 'cross_corrector'];
+  studentTableColumnsDisplay = ['Students', 'School Origin', 'School Correcting', 'Cross Corrector'];
+
+  constructor(
+    private api: ApiService
+  ) { }
 
   ngOnInit(): void {
+    try {
+      this.getAllStudents();
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  async getAllStudents() {
+    try {
+      const res = this.api.getAllStudent().subscribe(data => {
+        this.studentData.data = data;
+        this.studentData.data.forEach(e => {
+          e.students = `${e.student_id.first_name} ${e.student_id.last_name}`;
+          e.school_origin = e.school_origin_id.short_name;
+        });
+        console.log(this.studentData.data);
+      });
+    } catch (error) {
+      console.log(error);
+    }
   }
 
 }
